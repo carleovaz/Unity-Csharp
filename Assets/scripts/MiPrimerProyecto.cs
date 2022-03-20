6182409a6fbfa034e7fbc88bbab2e21283f03112
carleovaz <73903458+carleovaz@users.noreply.github.com>
2022-03-20 02:20:09 +0100

    Add files via upload

---
+++ Assets/scripts/MiPrimerProyecto.cs
@@ -0,0 +1,19 @@
+//NUESTRO SCRIPT VA A USAR EL MOTOR DE UNITY
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimerProyecto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
